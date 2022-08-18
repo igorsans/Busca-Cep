818b8e261d3790e6c0766df2e82df3b8d4aba8cc
alonso-estevam <alonso_estevam@outlook.com>
2022-08-18 20:21:43 -0300

    feat: add componente Card

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,19 +1,52 @@
-import React from 'react'
+import { useState } from 'react'
 import Input from '../Input/Input'
 import Button from '../Button/Button'
+import Card from '../Card/Card'
 
-const buscar = (e) => {
-    e.preventDefault()
 
-}
 
 const Form = () => {
+
+  const [cep, setCep] = useState('')
+
+  const [historico, setHistorico] = useState([])
+
+  const [card, setCard] = useState('')
+
+  const handleChange = (e) => {
+    setCep(e.target.value)
+  }
+
+  async function handleRequisicao(){
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    const json = await response.json()
+    console.log(json);
+    setCard(json)
+    setHistorico([...historico, json])
+}
+
+  const buscar = (e) => {
+      e.preventDefault()
+      setCard('')
+      handleRequisicao()
+  }
+
   return (
-    <form onSubmit={buscar}>
-        <h1>Buscar</h1>
-        <Input/>
-        <Button/>
-    </form>
+    <div>
+      <form onSubmit={buscar}>
+          <h1>Buscar</h1>
+          <Input handleChange = {handleChange}/>
+          <Button/>
+      </form>
+
+      <div>
+        { card ? <Card rua={card.logradouro } bairro={card.bairro} cidade={card.localidade}/> : ''}
+      </div>
+
+      <h2>historico</h2>
+      { historico ?  historico.map((item) => <Card key={item.cep} rua={item.logradouro} bairro={item.bairro} cidade={item.localidade}/>) : ''}
+
+    </div>
   )
 }
 
